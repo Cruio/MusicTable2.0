53d3de1ef91e8bc9abc02052e675890440e67cdf
Mathias Sand Kristensen <mskr16@student.aau.dk>
2017-11-23 19:21:23 +0100

    Font

--- MusicTable2.0/Form1.Designer.cs
+++ MusicTable2.0/Form1.Designer.cs
@@ -188,10 +188,10 @@
             this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(1003, 9);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1001, 9);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(144, 37);
+            this.label5.Size = new System.Drawing.Size(132, 33);
             this.label5.TabIndex = 8;
             this.label5.Text = "Niveauer";
             // 
